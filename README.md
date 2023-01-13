878d49b9ead1f9f5aa68e78292a586d8bf1fbdbc
swatishchoudhury <swatishchoudhury@gmail.com>
2023-01-13 22:44:00 +0530

    ptb v20

---
+++ README.md
@@ -0,0 +1,7 @@
+##
+**Disclaimer**
+
+Everyone is suggested to use the official results portal.
+The data provided (here) is **PUBLICALLY** available. It was made for a seminar and is intended for the convenience of students and other users. The bot is not affiliated with, endorsed, or sponsored by the university. While every effort has been made to ensure the accuracy of the information provided, the creators of this bot cannot guarantee that it is completely error-free. The creators of this bot will **NOT** be held liable for any inaccuracies, errors, or omissions in the information provided.
+##
+
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,61 +1,109 @@
 import os
+import requests
+from bs4 import BeautifulSoup
 import base64
 
-from telegram import Update
+from telegram import (
+    Update,
+    WebAppInfo,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.constants import ParseMode
 from telegram.ext import (
-    Updater,
+    ContextTypes,
     CommandHandler,
-    CallbackContext,
     MessageHandler,
-    Filters,
+    filters,
+    Application,
+    ApplicationBuilder,
 )
 
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        """Format: <batch> <sem> <roll no.>
-                              Eg: 2020-23 1ST SV2121***"""
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        """Send roll number in this format: `<batch> <semester> <roll no\.>`
+Example: `2020-23 1ST SV2121***`
+
+Source: https://github\.com/swatishchoudhury/BUresultsbot""",
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.MARKDOWN_V2,
     )
 
 
-def pdf(update: Update, context: CallbackContext):
-    text = update.message.text
-    msg = text.split(" ")
+async def pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        text = update.message.text
+        msg = text.split(" ")
+        text0_bytes = msg[0].encode("ascii")
+        base64_bytes = base64.b64encode(text0_bytes)
+        base640_text = base64_bytes.decode("ascii")
 
-    text0_bytes = msg[0].encode("ascii")
-    base64_bytes = base64.b64encode(text0_bytes)
-    base640_text = base64_bytes.decode("ascii")
+        text1_bytes = msg[1].encode("ascii")
+        base64_bytes = base64.b64encode(text1_bytes)
+        base641_text = base64_bytes.decode("ascii")
 
-    text1_bytes = msg[1].encode("ascii")
-    base64_bytes = base64.b64encode(text1_bytes)
-    base641_text = base64_bytes.decode("ascii")
+        text2_bytes = msg[2].encode("ascii")
+        base64_bytes = base64.b64encode(text2_bytes)
+        base642_text = base64_bytes.decode("ascii")
 
-    text2_bytes = msg[2].encode("ascii")
-    base64_bytes = base64.b64encode(text2_bytes)
-    base642_text = base64_bytes.decode("ascii")
+        pdfurl = (
+            "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_pdf.php?regn_no="
+            + base642_text
+            + "&sem="
+            + base641_text
+            + "&batch="
+            + base640_text
+            + "&cmbType=Regular"
+        )
+        weburl = (
+            "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_db.php?type=GET_DETAILS&regn_no="
+            + msg[2]
+            + "&sem="
+            + msg[1]
+            + "&batch="
+            + msg[0]
+            + "&cmbType=Regular"
+        )
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    "Results",
+                    web_app=WebAppInfo(weburl),
+                )
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        url = weburl
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    url = (
-        "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_pdf.php?regn_no="
-        + base642_text
-        + "&sem="
-        + base641_text
-        + "&batch="
-        + base640_text
-        + "&cmbType=Regular"
-    )
-    update.message.reply_document(document=url)
-    print("file sent")
+        table = soup.find("table")
+
+        table_data = ""
+        for row in table.find_all("tr"):
+            cells = [cell.text for cell in row.find_all("td")]
+            table_data += "\t".join(cells) + "\n"
+
+        # Send the table data as a single message
+        await update.message.reply_text(
+            table_data, reply_markup=reply_markup, parse_mode=ParseMode.HTML
+        )
+        await update.message.reply_document(document=pdfurl)
+
+    except Exception as e:
+        print(e)
+        await update.message.reply_text("Error!")
 
 
 def main():
-    updater = Updater(token=os.environ["bot_api"])
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(token=os.environ.get("BOT_API")).build()
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, pdf))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, pdf))
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 
 if __name__ == "__main__":
