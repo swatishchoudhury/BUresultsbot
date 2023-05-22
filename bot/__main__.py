ef615ee3906d5b2966092f882aa70255d31a623a
swatishchoudhury <swatishchoudhury@gmail.com>
2023-05-22 23:42:56 +0530

    Added try/except

--- bot/__main__.py
+++ bot/__main__.py
@@ -31,33 +31,24 @@ Source: https://github\.com/swatishchoudhury/BUresultsbot""",
     )
 
 
-async def pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def result(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    text = text.upper()
+    msg = text.split(" ")
+
+    batch_bytes = msg[0].encode("ascii")
+    base64_bytes = base64.b64encode(batch_bytes)
+    batch = base64_bytes.decode("ascii")
+
+    sem_bytes = msg[1].encode("ascii")
+    base64_bytes = base64.b64encode(sem_bytes)
+    sem = base64_bytes.decode("ascii")
+
+    roll_bytes = msg[2].encode("ascii")
+    base64_bytes = base64.b64encode(roll_bytes)
+    roll = base64_bytes.decode("ascii")
+
     try:
-        text = update.message.text
-        text = text.upper()
-        msg = text.split(" ")
-
-        batch_bytes = msg[0].encode("ascii")
-        base64_bytes = base64.b64encode(batch_bytes)
-        batch = base64_bytes.decode("ascii")
-
-        sem_bytes = msg[1].encode("ascii")
-        base64_bytes = base64.b64encode(sem_bytes)
-        sem = base64_bytes.decode("ascii")
-
-        roll_bytes = msg[2].encode("ascii")
-        base64_bytes = base64.b64encode(roll_bytes)
-        roll = base64_bytes.decode("ascii")
-
-        pdfurl = (
-            "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_pdf.php?regn_no="
-            + roll
-            + "&sem="
-            + sem
-            + "&batch="
-            + batch
-            + "&cmbType=Regular"
-        )
         weburl = (
             "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_db.php?type=GET_DETAILS&regn_no="
             + msg[2]
@@ -90,11 +81,23 @@ async def pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             table_data, reply_markup=reply_markup, parse_mode=ParseMode.HTML
         )
-        await update.message.reply_document(document=pdfurl)
-
     except Exception as e:
         print(e)
-        await update.message.reply_text("Error!")
+
+    try:
+        pdfurl = (
+            "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_pdf.php?regn_no="
+            + roll
+            + "&sem="
+            + sem
+            + "&batch="
+            + batch
+            + "&cmbType=Regular"
+        )
+        await update.message.reply_document(document=pdfurl)
+    except Exception as e:
+        print(e)
+        await update.message.reply_text("Unable to fetch result.")
 
 
 def main():
@@ -102,7 +105,7 @@ def main():
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, pdf))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, result))
 
     application.run_polling()
 
