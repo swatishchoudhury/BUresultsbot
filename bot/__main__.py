304a98f52a10e83066afdb0e766c96a54f23a3a7
swatishchoudhury <swatishchoudhury@gmail.com>
2023-05-30 20:00:41 +0530

    Update pdf sending method, parsemode

--- bot/__main__.py
+++ bot/__main__.py
@@ -16,39 +16,41 @@ from telegram.ext import (
     MessageHandler,
     filters,
     Application,
-    ApplicationBuilder,
 )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        """Send roll number in this format: `<batch> <semester> <roll no\.>`
-Example: `2020-23 1ST SV2121***`
+        """Send roll number in this format: <code>batch semester rollno.</code>
+Example: <code>2020-23 1ST SV2121***</code>
 
-Source: https://github\.com/swatishchoudhury/BUresultsbot""",
+Source: https://github.com/swatishchoudhury/BUresultsbot""",
         disable_web_page_preview=True,
-        parse_mode=ParseMode.MARKDOWN_V2,
+        parse_mode=ParseMode.HTML,
     )
 
 
+folder = ".temp"
+
+
 async def result(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    text = text.upper()
-    msg = text.split(" ")
-
-    batch_bytes = msg[0].encode("ascii")
-    base64_bytes = base64.b64encode(batch_bytes)
-    batch = base64_bytes.decode("ascii")
-
-    sem_bytes = msg[1].encode("ascii")
-    base64_bytes = base64.b64encode(sem_bytes)
-    sem = base64_bytes.decode("ascii")
-
-    roll_bytes = msg[2].encode("ascii")
-    base64_bytes = base64.b64encode(roll_bytes)
-    roll = base64_bytes.decode("ascii")
-
     try:
+        text = update.message.text
+        text = text.upper()
+        msg = text.split(" ")
+
+        batch_bytes = msg[0].encode("ascii")
+        base64_bytes = base64.b64encode(batch_bytes)
+        batch = base64_bytes.decode("ascii")
+
+        sem_bytes = msg[1].encode("ascii")
+        base64_bytes = base64.b64encode(sem_bytes)
+        sem = base64_bytes.decode("ascii")
+
+        roll_bytes = msg[2].encode("ascii")
+        base64_bytes = base64.b64encode(roll_bytes)
+        roll = base64_bytes.decode("ascii")
+
         weburl = (
             "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_db.php?type=GET_DETAILS&regn_no="
             + msg[2]
@@ -81,10 +83,7 @@ async def result(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             table_data, reply_markup=reply_markup, parse_mode=ParseMode.HTML
         )
-    except Exception as e:
-        print(e)
 
-    try:
         pdfurl = (
             "https://berhampuruniversity.silicontechlab.com/buerp/build/examination/mark_sheet_pdf.php?regn_no="
             + roll
@@ -94,10 +93,37 @@ async def result(update: Update, context: ContextTypes.DEFAULT_TYPE):
             + batch
             + "&cmbType=Regular"
         )
-        await update.message.reply_document(document=pdfurl)
-    except Exception as e:
-        print(e)
-        await update.message.reply_text("Unable to fetch result.")
+        chat_id = update.effective_message.chat_id
+
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        response = requests.get(pdfurl)
+        if response.status_code == 200:
+            filename = os.path.join(f"./{folder}", f"{msg[2]}.pdf")
+            with open(filename, "wb") as file:
+                file.write(response.content)
+        else:
+            await update.message("Unable to send pdf.")
+            return
+        await context.bot.send_document(
+            document=open(f"./{folder}/{msg[2]}.pdf", "rb"), chat_id=chat_id
+        )
+        files = os.listdir(f"./{folder}")
+        for file in files:
+            os.remove(f"./{folder}/{file}")
+        os.rmdir(f"./{folder}")
+    except IndexError:
+        await update.message.reply_text(
+            """Error! Please ensure that you are sending the query in the correct format:
+<code>batch semester rollno.</code>
+Example: <code>2020-23 1ST SV2121***</code>
+            """,
+            parse_mode=ParseMode.HTML,
+        )
+    except Exception:
+        await update.message.reply_text(
+            "Error! Probably the result server is acting up!"
+        )
 
 
 def main():
